254a0cf79b8bb6ace83286d508d331e4edb4b70f
Simon Hurault <simon.hurault@orange.fr>
2019-08-09 22:04:08 +0200

    add confirm_command.html file

--- api/migrations/0008_auto_20190804_1016.py
+++ api/migrations/0008_auto_20190804_1016.py
@@ -17,7 +17,7 @@ class Migration(migrations.Migration):
         migrations.AddField(
             model_name='command',
             name='author',
-            field=models.ForeignKey(default=User.objects.get(username="Florent").id, on_delete=django.db.models.deletion.CASCADE, related_name='command_author', to=settings.AUTH_USER_MODEL),
+            field=models.ForeignKey(default=User.objects.get(username="Simon").id, on_delete=django.db.models.deletion.CASCADE, related_name='command_author', to=settings.AUTH_USER_MODEL),
             preserve_default=False,
         ),
         migrations.AddField(
