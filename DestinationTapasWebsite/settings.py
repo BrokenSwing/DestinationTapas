0d10e6dc9311ac261f709a8877c671431347b682
unknown <allocompte@hotmail.fr>
2019-09-15 12:45:19 +0200

    Add STATIC_ROOT

--- DestinationTapasWebsite/settings.py
+++ DestinationTapasWebsite/settings.py
@@ -130,6 +130,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = '/static/'
 
 import django_heroku
 django_heroku.settings(locals())
